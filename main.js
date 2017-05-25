a13beaa17875338fc626a4dca056dcfbcc44d01e
Juhan Hennoste <juhan.hennoste@mhg.tartu.ee>
2017-05-25 15:30:27 +0300

    Add input for pyramid height and apothem. Restructure pyramid update logic

--- main.js
+++ main.js
@@ -1,20 +1,32 @@
-function getNSidedPyramid(sidecount) {
-	var tip = seen.P(0,1,0);
+var sidecount = 7;
+var height = 1.5;
+var apothem = 1;
+
+function getNSidedPyramid(n, h, R) {
+	var tip = seen.P(0, 2 * h / 3, 0);
 	var bottom = [];
-	for (var i = 0; i < sidecount; i++) {
-		var angle = i * Math.PI * 2.0 / sidecount;
-		bottom.push(seen.P(Math.cos(angle), -0.5, Math.sin(angle)));
+	for (var i = 0; i < n; i++) {
+		var angle = i * Math.PI * 2.0 / n;
+		bottom.push(seen.P(Math.cos(angle) * R, -h / 3, Math.sin(angle) * R));
 	}
 	var sides = [];
-	for (var i = 0; i < sidecount; i++) {
-		sides.push(new seen.Surface([bottom[(i + 1) % sidecount], bottom[i], tip]));
+	for (var i = 0; i < n; i++) {
+		sides.push(new seen.Surface([bottom[(i + 1) % n], bottom[i], tip]));
 	}
 	sides.push(new seen.Surface(bottom));
 	return sides;
 }
 
-function updatePyramid(sidecount) {
-	shape = new seen.Shape("terejaistuge", getNSidedPyramid(parseInt(sidecount))).scale(height * 0.3);
+function apothemToSpoke(m, n) {
+	var angle = Math.PI / n;
+	return m / Math.sin(Math.PI / 2.0 - angle);
+}
+
+function updatePyramid() {
+	var spoke = apothemToSpoke(apothem, sidecount)
+	console.log(sidecount, height, apothem, spoke)
+
+	shape = new seen.Shape("terejaistuge", getNSidedPyramid(sidecount, height, spoke)).scale(canvasheight * 0.3);
 	scene.model.children[0] = shape;
 	context.render()
 }
@@ -25,31 +37,30 @@ var ratio = (function() {
 	var backingStoreRatio = seencontext.webkitBackingStorePixelRatio || seencontext.mozBackingStorePixelRatio || seencontext.msBackingStorePixelRatio || seencontext.oBackingStorePixelRatio || seencontext.backingStorePixelRatio || 1;
 	return devicePixelRatio / backingStoreRatio;
 })();
-var width = document.getElementById("seen-canvas").clientWidth * ratio;
-var height = document.getElementById("seen-canvas").clientHeight * ratio;
-document.getElementById("seen-canvas").width = width;
-document.getElementById("seen-canvas").height = height;
+var canvaswidth = document.getElementById("seen-canvas").clientWidth * ratio;
+var canvasheight = document.getElementById("seen-canvas").clientHeight * ratio;
+document.getElementById("seen-canvas").width = canvaswidth;
+document.getElementById("seen-canvas").height = canvasheight;
 
 var overlaysurface = new seen.Surface([seen.P(0,1,0),seen.P(1,-0.5,0),seen.P(0,-0.5,0)]).stroke(new seen.Color(192, 57, 43, 255));
 overlaysurface.fillMaterial = null;
 overlaysurface["stroke-width"] = 4;
 
-var shape = new seen.Shape("terejaistuge", getNSidedPyramid(7)).scale(height * 0.3);
-var overlayshape = new seen.Shape("pealminek", [overlaysurface]).scale(height * 0.3);
-//var unitcube = seen.Shapes.unitcube().scale(height * 0.2);
+var shape = new seen.Shape("terejaistuge", getNSidedPyramid(sidecount, height, apothemToSpoke(apothem, sidecount))).scale(canvasheight * 0.3);
+var overlayshape = new seen.Shape("pealminek", [overlaysurface]).scale(canvasheight * 0.3);
 
 //seen.Colors.randomSurfaces2(shape);
 
 var overlayscene = new seen.Scene({
-	model: seen.Models.default().add(overlayshape),
-	viewport: seen.Viewports.center(width, height),
+	model: seen.Models.default(),//.add(overlayshape),
+	viewport: seen.Viewports.center(canvaswidth, canvasheight),
 	shader: seen.Shaders.flat(),
 	cullBackfaces: false
 });
 
 var scene = new seen.Scene({
 	model: seen.Models.default().add(shape),//.add(unitcube),
-	viewport: seen.Viewports.center(width, height),
+	viewport: seen.Viewports.center(canvaswidth, canvasheight),
 	shader: seen.Shaders.diffuse()
 });
 
