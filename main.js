01c445f834fa4040f7dfe4fb6868d37039401257
Juhan Hennoste <juhan.hennoste@mhg.tartu.ee>
2017-06-01 15:04:14 +0300

    Add various calculations and display area and volume. Add automatic scaling of model.

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
-var sidecount = 7;
-var height = 1.5;
-var apothem = 1;
+var canvaswidth, canvasheight;
+var shape;
+
+window.onresize = function(event) {
+	rescaleCanvas();
+	updatePyramid();
+}
+
+function rescaleCanvas() {
+	canvaswidth = document.getElementById("seen-canvas").clientWidth * ratio;
+	canvasheight = document.getElementById("seen-canvas").clientHeight * ratio;
+	document.getElementById("seen-canvas").width = canvaswidth;
+	document.getElementById("seen-canvas").height = canvasheight;
+
+	scene.viewport = seen.Viewports.center(canvaswidth, canvasheight);
+}
 
 function getNSidedPyramid(n, h, R) {
 	var tip = seen.P(0, 2 * h / 3, 0);
@@ -17,59 +30,89 @@ function getNSidedPyramid(n, h, R) {
 	return sides;
 }
 
-function apothemToSpoke(m, n) {
+function calculateSpoke(r, n) {
 	var angle = Math.PI / n;
-	return m / Math.sin(Math.PI / 2.0 - angle);
+	return r / Math.cos(angle);
+}
+
+function calculateBaseedge(r, n) {
+	var angle = Math.PI / n;
+	return Math.tan(angle) * r * 2;
+}
+
+function calculateApothem(r, h) {
+	return Math.sqrt(r * r + h * h);
+}
+
+function recalculatePyramid(){
+	spoke = calculateSpoke(baseapothem, sidecount);
+	apothem = calculateApothem(baseapothem, height);
+	baseedge = calculateBaseedge(baseapothem, sidecount);
+	//console.log(baseedge, baseapothem, spoke);
+
+	var basearea = baseedge * baseapothem * sidecount / 2; 
+	var sidearea = baseedge * apothem * sidecount / 2;
+	area = basearea + sidearea;
+	volume = basearea * height / 3;
+
+	document.getElementById("area").value = +area.toFixed(10);
+	document.getElementById("volume").value = +volume.toFixed(10);
 }
 
 function updatePyramid() {
-	var spoke = apothemToSpoke(apothem, sidecount)
-	console.log(sidecount, height, apothem, spoke)
+	recalculatePyramid();
 
-	shape = new seen.Shape("terejaistuge", getNSidedPyramid(sidecount, height, spoke)).scale(canvasheight * 0.3);
+	shape = new seen.Shape("terejaistuge", getNSidedPyramid(sidecount, height, spoke)).scale(canvasheight * 0.3 / Math.max(spoke, height / 2));
 	scene.model.children[0] = shape;
-	context.render()
+	context.render();
 }
 
-var ratio = (function() {
-	var seencontext = document.getElementById("seen-canvas").getContext("2d");
-	var devicePixelRatio = window.devicePixelRatio || 1;
-	var backingStoreRatio = seencontext.webkitBackingStorePixelRatio || seencontext.mozBackingStorePixelRatio || seencontext.msBackingStorePixelRatio || seencontext.oBackingStorePixelRatio || seencontext.backingStorePixelRatio || 1;
-	return devicePixelRatio / backingStoreRatio;
-})();
-var canvaswidth = document.getElementById("seen-canvas").clientWidth * ratio;
-var canvasheight = document.getElementById("seen-canvas").clientHeight * ratio;
-document.getElementById("seen-canvas").width = canvaswidth;
-document.getElementById("seen-canvas").height = canvasheight;
-
-var overlaysurface = new seen.Surface([seen.P(0,1,0),seen.P(1,-0.5,0),seen.P(0,-0.5,0)]).stroke(new seen.Color(192, 57, 43, 255));
-overlaysurface.fillMaterial = null;
-overlaysurface["stroke-width"] = 4;
-
-var shape = new seen.Shape("terejaistuge", getNSidedPyramid(sidecount, height, apothemToSpoke(apothem, sidecount))).scale(canvasheight * 0.3);
-var overlayshape = new seen.Shape("pealminek", [overlaysurface]).scale(canvasheight * 0.3);
-
-//seen.Colors.randomSurfaces2(shape);
+var sidecount = 7;
+var height = 1.5;
+var baseapothem = 1;
+var spoke, apothem, baseedge;
+var area, volume;
+recalculatePyramid();
 
 var overlayscene = new seen.Scene({
 	model: seen.Models.default(),//.add(overlayshape),
-	viewport: seen.Viewports.center(canvaswidth, canvasheight),
 	shader: seen.Shaders.flat(),
 	cullBackfaces: false
 });
 
 var scene = new seen.Scene({
-	model: seen.Models.default().add(shape),//.add(unitcube),
-	viewport: seen.Viewports.center(canvaswidth, canvasheight),
+	model: seen.Models.default(),//.add(unitcube),
 	shader: seen.Shaders.diffuse()
 });
+
+var ratio = (function() {
+	var seencontext = document.getElementById("seen-canvas").getContext("2d");
+	var devicePixelRatio = window.devicePixelRatio || 1;
+	var backingStoreRatio = seencontext.webkitBackingStorePixelRatio
+						|| seencontext.mozBackingStorePixelRatio
+						|| seencontext.msBackingStorePixelRatio
+						|| seencontext.oBackingStorePixelRatio
+						|| seencontext.backingStorePixelRatio
+						|| 1;
+	return devicePixelRatio / backingStoreRatio;
+})();
+rescaleCanvas();
+
+//var overlaysurface = new seen.Surface([seen.P(0,1,0),seen.P(1,-0.5,0),seen.P(0,-0.5,0)]).stroke(new seen.Color(192, 57, 43, 255));
+//overlaysurface.fillMaterial = null;
+//overlaysurface["stroke-width"] = 4;
+
+//var overlayshape = new seen.Shape("pealminek", [overlaysurface]).scale(canvasheight * 0.3);
+
+//seen.Colors.randomSurfaces2(shape);
 
 var context = seen.Context("seen-canvas");
 context.sceneLayer(scene);
 context.sceneLayer(overlayscene);
 context.ctx.lineCap = "round";
 context.ctx.lineJoin = "round";
-context.render();
+
+updatePyramid()
 
 var dragger = new seen.Drag("seen-canvas", {
 	inertia: true
